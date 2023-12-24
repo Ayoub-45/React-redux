31b4e9e99505d37409c7cd2eae1b2bae9c3a7b6d
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2023-12-24 23:18:55 +0100

    Finish the counter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useDispatch, useSelector } from "react-redux";
+import { actions } from "./store/store";
+import { useState } from "react";
 function App() {
   const counter = useSelector((state) => state.count);
   const dispatch = useDispatch();
+  const [number, setNumber] = useState(0);
   console.log(counter);
   return (
     <div className="App">
@@ -9,15 +12,22 @@ function App() {
         <h1>counter:{counter}</h1>
         <button
           onClick={() => {
-            dispatch({ type: "INC" });
+            dispatch(actions.increment());
           }}
         >
           Increment
         </button>
-        <button onClick={() => dispatch({ type: "ADD", payload: 10 })}>
-          Add by 10
+        <input
+          type="number"
+          value={number}
+          onChange={(e) => {
+            setNumber(e.target.value);
+          }}
+        />
+        <button onClick={() => dispatch(actions.addBy(Number(number)))}>
+          Add by {number}
         </button>
-        <button onClick={() => dispatch({ type: "DEC" })}>Decrement</button>
+        <button onClick={() => dispatch(actions.decrement())}>Decrement</button>
       </header>
     </div>
   );
--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,20 @@
-import { createStore } from "@reduxjs/toolkit";
-const reducerFn = (state = { count: 0 }, action) => {
-  //Synchronous function
-  //We should not mutate the original state
-  switch (action.type) {
-    case "INC":
-      return { ...state, count: state.count + 1 };
-    case "DEC":
-      return { ...state, count: state.count - 1 };
-    case "ADD":
-      return { ...state, count: state.count + action.payload };
-    default:
-      return state;
-  }
-};
-export const store = createStore(reducerFn);
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: { count: 0 },
+  reducers: {
+    increment(state, action) {
+      state.count++;
+    },
+    decrement(state, action) {
+      state.count--;
+    },
+    addBy(state, action) {
+      state.count += action.payload;
+    },
+  },
+});
+export const actions = counterSlice.actions;
+export const store = configureStore({
+  reducer: counterSlice.reducer,
+});
